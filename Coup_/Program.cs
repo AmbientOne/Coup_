e71d05363353258ccd7a981b0723b79da4307446
AmbientOne <abdirahman.abdi1@gmail.com>
2021-01-10 18:32:50 -0800

    Adding initial files

---
+++ Coup_/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my coup game!");
+        }
+    }
+}
